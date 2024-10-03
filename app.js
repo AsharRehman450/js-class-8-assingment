3cc5230664bfa91076dcab8f14625620ef2c8be5
AsharRehman450 <asharrehmanali@gmail.com>
2024-10-03 12:52:14 +0500

    add files and folder

---
+++ app.js
@@ -0,0 +1,12 @@
+// var passwordLength = 10;
+
+// var input =" ABCDEFGHIJKLMNOPQRSTUVWXYZabscdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+// var password = "";
+
+// for (i = 0; i < passwordLength; i++) {
+//     password = password + input.charAt(Math.floor(Math.random()*input.length))
+
+// }
+
+// console.log("Generated Password:",password)t
